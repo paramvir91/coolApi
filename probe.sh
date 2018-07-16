31ccfd91278299b9a3045aab9c28edf28792093f
Paramvir Singh <p.singh@sevensenders.com>
2018-07-16 19:31:12 +0200

    final adjustments

--- probe.sh
+++ probe.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-CHECK=$(curl 'http://localhost:80/healthcheck' -s -o /dev/null -w "response_code: %{http_code}")
+CHECK=$(curl 'http://localhost:80/healthcheck' -s -o /dev/null -w "response_code: %{http_code}\n connect_time: %{time_connect}")
 while true
  do
-  echo $CHECK > File.txt
+  echo $CHECK >> file.txt
+  sleep 2s
  done
